6a0c218b760745e374b4459fb2656777422573d5
yousufsha <usufshameem47@gmail.com>
2026-08-12 13:45:43 +0300

    vs 1

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -8,16 +8,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pageElement.HomePage;
-import pageElement.ProductPage;
-import pageElement.SearchPage;
 
 public class BaseClass {
 
